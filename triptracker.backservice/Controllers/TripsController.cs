be09a420eadc083f7326f23403d3b7c004b9ce30
JitheshKRamesh <jitheshkramesh@gmail.com>
2019-07-04 12:39:45 +0400

    First module

---
+++ triptracker.backservice/Controllers/TripsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TripTracker.BackService.Models;
+
+namespace TripTracker.BackService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        private Repository _repository;
+        public TripsController(Repository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Trip> Get()
+        {
+            return _repository.Get();
+        }
+
+        [HttpGet("{Id}")]
+        public Trip Get(int id)
+        {
+            return _repository.Get(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Trip value)
+        {
+            _repository.Add(value);
+        }
+
+        [HttpPut]
+        public void Put(int id, [FromBody] Trip value)
+        {
+            _repository.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(id);
+        }
+    }
+}
---
+++ triptracker.backservice/Models/Repository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTracker.BackService.Models
+{
+    public class Repository
+    {
+        private List<Trip> MyTrips = new List<Trip>
+        {
+            new Trip
+            {
+                Id = 1,
+                Name = "MVP Submit",
+                StartDate = new DateTime(2019, 3, 4),
+                EndDate = new DateTime(2019, 05, 15)
+
+            },
+            new Trip
+            {
+                Id = 2,
+                Name = "MVP Submit 2",
+                StartDate = new DateTime(2019, 3, 5),
+                EndDate = new DateTime(2019, 05, 17)
+            },
+            new Trip
+            {
+                Id = 3,
+                Name = "MVP Submit 3",
+                StartDate = new DateTime(2019, 3, 6),
+                EndDate = new DateTime(2019, 05, 18)
+            },
+            new Trip
+            {
+                Id = 4,
+                Name = "MVP Submit 4",
+                StartDate = new DateTime(2019, 3, 7),
+                EndDate = new DateTime(2019, 05, 25)
+            },
+
+            new Trip
+            {
+                Id = 5,
+                Name = "MVP Submit 5",
+                StartDate = new DateTime(2019, 3, 8),
+                EndDate = new DateTime(2019, 05, 19)
+            }
+        };
+
+        public List<Trip> Get()
+        {
+            return MyTrips;
+        }
+
+        public Trip Get(int id)
+        {
+            return MyTrips.FirstOrDefault(t => t.Id == id);
+        }
+        public void Add(Trip newTrip)
+        {
+            MyTrips.Add(newTrip);
+        }
+
+        public void Update(Trip tripToUpdate)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == tripToUpdate.Id));
+            Add(tripToUpdate);
+        }
+
+        public void Remove(int id)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == id));
+        }
+    }
+}
